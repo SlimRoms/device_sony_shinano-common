CyanogenMod/android_device_sony_rhine-common

Add PCC calibration interface
 * This will be used by the new LiveDisplay feature.

Change-Id: I1c7e8547d5305a8c8031e972d1513fa8906908e7
Signed-off-by: Chirayu Desai <chirayudesai1@gmail.com>

BoardConfigCommon.mk
@@ -41,7 +41,7 @@ BOARD_CAMERA_HAVE_ISO := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS := device/sony/rhine-common/cmhw
+BOARD_HARDWARE_CLASS += device/sony/rhine-common/cmhw
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony

SlimRoms/device_sony_shinano-common

BoardConfigCommon.mk
@@ -46,7 +46,7 @@ BOARD_CAMERA_HAVE_ISO := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 
 # CM Hardware
-BOARD_HARDWARE_CLASS := device/sony/shinano-common/cmhw
+BOARD_HARDWARE_CLASS += device/sony/shinano-common/cmhw
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony